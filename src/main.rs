8da393f035f17ace4b9e4f63edd388e0e40987e6
AlexGarrity <garrity2799@gmail.com>
2025-10-07 02:37:23 +0100

    refactor: General clean-up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use csv;
-
 trait ModelData<const INPUTS: usize, const OUTPUTS: usize> {
     fn to_input(&self) -> [f32; INPUTS];
     fn to_actual(&self) -> [f32; OUTPUTS];
@@ -8,131 +6,89 @@ trait ModelData<const INPUTS: usize, const OUTPUTS: usize> {
 struct Model<const INPUTS: usize, const OUTPUTS: usize, T: ModelData<INPUTS, OUTPUTS>> {
     inputs: [Neuron<1>; INPUTS],
     outputs: [Neuron<INPUTS>; OUTPUTS],
-    data: std::marker::PhantomData<T>
+    data: std::marker::PhantomData<T>,
 }
 
-impl<const INPUTS: usize, const OUTPUTS: usize, T: ModelData<INPUTS, OUTPUTS>> Model<INPUTS, OUTPUTS, T> {
+impl<const INPUTS: usize, const OUTPUTS: usize, T: ModelData<INPUTS, OUTPUTS>>
+    Model<INPUTS, OUTPUTS, T>
+{
     fn new() -> Self {
         Self {
-            inputs: core::array::from_fn(|_| {Neuron::new()}),
-            outputs: core::array::from_fn(|_| {Neuron::new()}),
-            data: std::marker::PhantomData
+            inputs: core::array::from_fn(|_| Neuron::new()),
+            outputs: core::array::from_fn(|_| Neuron::new()),
+            data: std::marker::PhantomData,
         }
     }
 
     fn predict(&self, data: &T) -> [f32; OUTPUTS] {
-        let mut predictions = [0.0; INPUTS];
         let input = data.to_input();
-        for i in 0..INPUTS {
-            predictions[i] = self.inputs[i].predict([input[i]])
-        }
-        
-        let mut outputs = [0.0; OUTPUTS];
-        for i in 0..OUTPUTS {
-            outputs[i] = self.outputs[i].predict(predictions);
-        }
-        
-        outputs
+        let predictions = core::array::from_fn(|i| self.inputs[i].predict([input[i]]));
+
+        core::array::from_fn(|i| self.outputs[i].predict(predictions))
     }
 
     fn error(&self, data: &T) -> f32 {
         let predictions = self.predict(data);
         let actual = data.to_actual();
-        let mut error = 0.0;
-        for i in 0..OUTPUTS {
-            error += predictions[i] - actual[i];
-        }
 
-        error
+        predictions.iter().zip(actual).map(|(p, a)| p - a).sum()
     }
 
     fn loss(&self, data: &T) -> f32 {
         let predictions = self.predict(data);
         let actual = data.to_actual();
-        let mut loss = 0.0;
-        for i in 0..OUTPUTS {
-            let x = predictions[i] - actual[i];
-            loss += x * x * 0.5;
-        }
-        
-        loss
+
+        predictions
+            .iter()
+            .zip(actual)
+            .map(|(p, a)| {
+                let x = p - a;
+                x * x * 0.5
+            })
+            .sum()
     }
 
     fn step(&mut self, data: &T, lr: f32) {
-        let mut predictions = [0.0; INPUTS];
         let input = data.to_input();
-        for i in 0..INPUTS {
-            predictions[i] = self.inputs[i].predict([input[i]]);
-        }
+        let predictions = core::array::from_fn(|i| self.inputs[i].predict([input[i]]));
 
         let error = self.error(data);
 
         for i in 0..OUTPUTS {
             self.outputs[i].update(predictions, lr, error);
         }
-        for i in 0..INPUTS {
-            self.inputs[i].update([input[i]], lr, error);
+        for (i, v) in input.iter().enumerate().take(INPUTS) {
+            self.inputs[i].update([*v], lr, error);
         }
     }
 }
 
 struct Neuron<const N: usize> {
     weight: [f32; N],
-    bias: f32
+    bias: f32,
 }
 
 impl<const N: usize> Neuron<N> {
     fn new() -> Self {
-        let mut weight = [0.0; N];
-        for i in 0..N {
-            weight[i] = rand::random_range(0.0..1.0);
-        }
-
-        Self {
-            weight,
-            bias: 0.0
-        }
+        let weight = core::array::from_fn(|_| rand::random_range(0.0..1.0));
+        Self { weight, bias: 0.0 }
     }
 
-    fn predict(&self, v: [f32; N]) -> f32 {
+    fn predict(&self, input: [f32; N]) -> f32 {
         let mut x = 0.0;
-        for i in 0..N {
-            x += self.weight[i] * v[i];
+        for (i, v) in input.iter().enumerate().take(N) {
+            x += self.weight[i] * v;
         }
 
         x + self.bias
     }
 
-    #[allow(unused)]
-    fn error(&self, v: [f32; N], actual: f32) -> f32 {
-        self.predict(v) - actual
-    }
-
-    #[allow(unused)]
-    fn loss(&self, v: [f32; N], actual: f32) -> f32 {
-        let x = self.predict(v) - actual;
-        0.5 * x * x
-    }
-
-    fn update(&mut self, v: [f32; N], lr: f32, error: f32) {
+    fn update(&mut self, input: [f32; N], lr: f32, error: f32) {
         let db = error;
-        self.bias = self.bias - lr * db;
+        self.bias -= lr * db;
 
-        for i in 0..N {
-            let dw = error * v[i];
-            self.weight[i] -= lr * dw;
-        }
-    }
-
-    #[allow(unused)]
-    fn step(&mut self, v: [f32; N], actual: f32, lr: f32) {
-        let error = self.error(v, actual);
-        let db = error;
-
-        self.bias = self.bias - lr * db;
-
-        for i in 0..N {
-            let dw = error * v[i];
+        for (i, v) in input.iter().enumerate().take(N) {
+            let dw = error * v;
             self.weight[i] -= lr * dw;
         }
     }
@@ -150,7 +106,7 @@ struct Game {
     eu_sales: f32,
     jp_sales: f32,
     other_sales: f32,
-    global_sales: f32
+    global_sales: f32,
 }
 
 impl ModelData<4, 1> for Game {
@@ -159,12 +115,12 @@ impl ModelData<4, 1> for Game {
             self.eu_sales,
             self.jp_sales,
             self.na_sales,
-            self.other_sales
+            self.other_sales,
         ]
     }
 
     fn to_actual(&self) -> [f32; 1] {
-        [ self.global_sales ]
+        [self.global_sales]
     }
 }
 
@@ -178,21 +134,19 @@ fn load_data() -> Vec<Game> {
         .records()
         .filter_map(|record| {
             if let Ok(r) = record {
-                Some(
-                    Game {
-                        rank: r.get(0).unwrap().parse::<f32>().unwrap(),
-                        name: String::from(r.get(1).unwrap()),
-                        platform: String::from(r.get(2).unwrap()),
-                        year: r.get(3).unwrap().parse().unwrap(),
-                        genre: String::from(r.get(4).unwrap()),
-                        publisher: String::from(r.get(5).unwrap()),
-                        na_sales: r.get(6).unwrap().parse().unwrap(),
-                        eu_sales: r.get(7).unwrap().parse().unwrap(),
-                        jp_sales: r.get(8).unwrap().parse().unwrap(),
-                        other_sales: r.get(9).unwrap().parse().unwrap(),
-                        global_sales: r.get(10).unwrap().parse().unwrap(),
-                    }
-                )
+                Some(Game {
+                    rank: r.get(0).unwrap().parse::<f32>().unwrap(),
+                    name: String::from(r.get(1).unwrap()),
+                    platform: String::from(r.get(2).unwrap()),
+                    year: r.get(3).unwrap().parse().unwrap(),
+                    genre: String::from(r.get(4).unwrap()),
+                    publisher: String::from(r.get(5).unwrap()),
+                    na_sales: r.get(6).unwrap().parse().unwrap(),
+                    eu_sales: r.get(7).unwrap().parse().unwrap(),
+                    jp_sales: r.get(8).unwrap().parse().unwrap(),
+                    other_sales: r.get(9).unwrap().parse().unwrap(),
+                    global_sales: r.get(10).unwrap().parse().unwrap(),
+                })
             } else {
                 None
             }
@@ -200,11 +154,8 @@ fn load_data() -> Vec<Game> {
         .collect::<Vec<Game>>()
 }
 
-fn average_loss(model: &Model<4,1, Game>, data: &Vec<Game>) -> f32 {
-    let total_loss =
-        data.iter().fold(0.0, |acc, game| {
-            acc + model.loss(game)
-        });
+fn average_loss(model: &Model<4, 1, Game>, data: &[Game]) -> f32 {
+    let total_loss = data.iter().fold(0.0, |acc, game| acc + model.loss(game));
     total_loss / (data.len() as f32)
 }
 
@@ -223,11 +174,18 @@ fn main() {
         if !average_loss.is_normal() {
             panic!("Loss is not normal");
         }
-        println!("Epoch {:5}, LR: {:0.2e}, Loss: {:2.8}", epoch, learning_rate, average_loss);
+        println!(
+            "Epoch {:5}, LR: {:0.2e}, Loss: {:2.8}",
+            epoch, learning_rate, average_loss
+        );
     }
 
-    for i in 0..5 {
-        println!("Prediction for {} sales: {} / {}", data[i].name, model.predict(&data[i])[0], data[i].global_sales as f32);
+    for d in data.iter().take(5) {
+        println!(
+            "Prediction for {} sales: {} / {}",
+            d.name,
+            model.predict(d)[0],
+            d.global_sales
+        );
     }
-    
 }
